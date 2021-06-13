d0deaa770f4966a1ae6d31f70fb83cc5872b257b
Youssef El Mzouri <youssefmzouri@gmail.com>
2021-06-13 21:16:58 +0200

    added icons to githubRepoCard component

--- src/pages/Projects/components/RepositoriesFromGithub.js
+++ src/pages/Projects/components/RepositoriesFromGithub.js
@@ -2,6 +2,11 @@ import React, {useContext} from 'react';
 import SessionContext from './../../../context/SessionContext';
 import useGithubRepositories from '../../../hooks/useGithubRepositories';
 import { makeStyles } from '@material-ui/core/styles';
+import CodeIcon from '@material-ui/icons/Code';
+import StarIcon from '@material-ui/icons/Star';
+import ShareIcon from '@material-ui/icons/Share';
+import LockIcon from '@material-ui/icons/Lock';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,6 +27,8 @@ const useStyles = makeStyles((theme) => ({
     },
     cardHeader: {
         textAlign: 'left',
+        display: 'flex',
+        alignContent: 'center',
         '& > a': {
             cursor: 'pointer',
             fontWeight: 'bold',
@@ -31,19 +38,34 @@ const useStyles = makeStyles((theme) => ({
     },
     cardDesc: {
         textAlign: 'left',
+        display: '-webkit-box',
+        '-webkit-line-clamp': '2',
+        '-webkit-box-orient': 'vertical',
+        overflow: 'hidden'
     },
     cardDetails: {
         display: 'flex',
         justifyContent: 'flex-start',
-        gap: '10px'
+        gap: '18px',
+        '& > span': {
+            display: 'flex',
+            alignContent: 'center',
+            alignItems: 'center',
+            gap: '5px',
+            fontSize: '14px',
+            '& > svg': {
+                fontSize: '14px',
+            }
+        }
     }
 }));
 
-const RepoComponent = ({key, repositoriy}) => {
+const RepoComponent = ({repositoriy}) => {
     const classes = useStyles();
     return (
         <div className={classes.repoCard}>
             <div className={classes.cardHeader}>
+                <CodeIcon aria-label="repository" fontSize="small"/>
                 <a href={repositoriy.html_url}>{repositoriy.name}</a>
             </div>
             {
@@ -54,8 +76,24 @@ const RepoComponent = ({key, repositoriy}) => {
                 : null
             }
             <div className={classes.cardDetails}>
-                <span>Stars: {repositoriy.stargazers_count}</span>
-                <span>Forks: {repositoriy.forks}</span>
+                <span>
+                    <StarIcon aria-label="Stars" fontSize="small"/>
+                    {repositoriy.stargazers_count}
+                </span>
+                <span>
+                    <ShareIcon aria-label="Forks" fontSize="small"/>
+                    {repositoriy.forks}
+                </span>
+                <span>
+                    <VisibilityIcon aria-label="Watchers" fontSize="small"/>
+                    {repositoriy.watchers}
+                </span>
+                <span>
+                    {repositoriy.private ?
+                        <LockIcon aria-label="Private" fontSize="small"/>
+                        : null
+                    }
+                </span>
             </div>
         </div>
     )
